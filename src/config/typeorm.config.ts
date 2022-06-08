02fea23a2beb6e94e3ce1e82af23dd7478270fc7
Sebastian <sebastian.gvg94@gmail.com>
2022-06-08 13:30:19 -0400

    Edit Config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,11 +4,11 @@ import {DataSource} from "typeorm";
 dotenv.config();
 
 export const typeOrmConfig = new DataSource({
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST || '/cloudsql/chek-app-352602:us-central1:chek-bank-service',
   type: 'postgres',
   port: 5432,
   username: 'postgres',
-  password: process.env.DATABASE_PASSWORD,
+  password: process.env.DATABASE_PASSWORD || 's1=iVC;uo]Q+IY1&',
   database: 'chek-bank-service',
   synchronize: false,
   entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
